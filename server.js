fbf1288315852ebf941372eab3e88b2232e64ce7
ailona <81778135+ailonag@users.noreply.github.com>
2025-01-13 13:34:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/texts', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/texts?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const textSchema = new mongoose.Schema({
     user: String,
@@ -23,6 +28,7 @@ app.post('/texts', async (req, res) => {
     const { user, text, color } = req.body;
     const newText = new Text({ user, text, color });
     await newText.save();
+    io.emit('newText', newText); // Emit an event to all connected clients
     res.send(newText);
 });
 
@@ -33,10 +39,18 @@ app.get('/texts', async (req, res) => {
 
 app.delete('/texts', async (req, res) => {
     await Text.deleteMany();
+    io.emit('clearTexts'); // Emit an event to all connected clients
     res.send({ message: 'All texts cleared' });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
